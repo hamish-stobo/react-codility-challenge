4de74b1d7442e1d5d55961614cc2a2792bc8ba27
HHSNZ <hh.stobo@gmail.com>
2020-03-20 12:12:24 +1300

    pass requirements of test, start adding test instructions to readme.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [likesCount, setLikesCount] = useState(100)
+  const [likesBtnClicked, setLikesBtnClicked] = useState(false)
+  const [dislikesCount, setDislikesCount] = useState(25)
+  const [dislikesBtnClicked, setDislikesBtnClicked] = useState(false)
+
+  const changeLikesCount = () => {
+    if(likesBtnClicked) {
+      if(likesCount === 100) return
+      setLikesCount(likesCount - 1)
+    } else if (!likesBtnClicked) {
+      if(dislikesBtnClicked) {
+        if(dislikesCount === 25) return
+        setDislikesCount(dislikesCount - 1)
+        setDislikesBtnClicked(!dislikesBtnClicked)
+      }
+      setLikesCount(likesCount + 1)
+    }
+    setLikesBtnClicked(!likesBtnClicked)
+  }
+
+  const changeDislikesCount = () => {
+    if(dislikesBtnClicked) {
+      if(dislikesCount === 25) return
+      setDislikesCount(dislikesCount - 1)
+    } else if (!dislikesBtnClicked) {
+      if(likesBtnClicked) {
+        if(likesCount === 100) return
+        setLikesCount(likesCount - 1)
+        setLikesBtnClicked(!likesBtnClicked)
+      }
+      setDislikesCount(dislikesCount + 1)
+    }
+    setDislikesBtnClicked(!dislikesBtnClicked)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style>
+      {`
+        .like-button, .dislike-button {
+            font-size: 1rem;
+            padding: 5px 10px;
+            color:   #585858;
+        }
+
+        .liked, .disliked {
+            font-weight: bold;
+            color: #1565c0;
+        }
+      `}
+      </style>
+      <div>
+        <h2>Like/Dislike</h2>
+      </div>
+      <button className={"likeButton" + (likesBtnClicked && " liked")} onClick={changeLikesCount}>Like | 
+        <span className="likes-counter">{likesCount}</span>
+      </button>
+      <button className={"dislikeButton" + (dislikesBtnClicked && " disliked")} onClick={changeDislikesCount}>Dislike | 
+      <span className="dislikes-counter">{dislikesCount}</span>
+      </button>
+    </>
   );
 }
 
